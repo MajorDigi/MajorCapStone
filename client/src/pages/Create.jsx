5648b9f1b5d332dca77cdcd674d87531194c53c1
MajorDigi <majormcneil3d@gmail.com>
2024-10-15 23:22:38 -0400

    Updated Home.jsx Create.jsx

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,14 +1,12 @@
 // client/src/pages/Create.jsx
-
-import React, { useContext, useState } from 'react'
-import axios from "axios"
+import React, { useContext, useState } from 'react';
+import axios from "axios";
 import { AuthContext } from '../authContext';
-import "../styles/create.css"
+import "../styles/create.css";
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../styles/create.css"
 
 const Create = () => {
     const navigate = useNavigate();
@@ -16,53 +14,51 @@ const Create = () => {
     const [files, setFiles] = useState("");
     const [info, setInfo] = useState({});
 
-    // set the usestate to the data user passed 
+    // Set the state to the data user passed 
     const handleChange = (e) => {
         setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
     }
 
-    // post the usestate to database
+    // Post the state to database
     const handleClick = async (e) => {
         e.preventDefault();
 
-        var newEntry
+        let newEntry;
 
         if (files) {
             const list = await Promise.all(Object.values(files).map(async (file) => {
                 const data = new FormData();
                 data.append("file", file);
-                data.append("upload_preset", "upload")
+                data.append("upload_preset", "upload"); // Your upload preset
+
+                // Use environment variable for Cloudinary URL
                 const uploadRes = await axios.post(
-                    "https://api.cloudinary.com/v1_1/<your_cloudinary_key>/image/upload",
-                    data, { withcredentials: false }
-                )
+                    `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`, // Use the cloud name from .env
+                    data, { withCredentials: false }
+                );
+
                 const { url } = uploadRes.data;
                 return url;
-            }))
-
+            }));
 
             newEntry = {
                 ...info, author: user._id, photos: list
-            }
+            };
 
-        }
-        else {
+        } else {
             newEntry = {
                 ...info, author: user._id
-            }
+            };
         }
-
 
         try {
-            const response = await axios.post('http://localhost:5500/api/entries/',
-                newEntry, {
+            const response = await axios.post('http://localhost:5500/api/entries/', newEntry, {
                 withCredentials: false
-            })
+            });
 
             navigate(`/view/${response?.data?._id}`);
-        }
-        catch (err) {
-            console.log(err)
+        } catch (err) {
+            console.log(err);
         }
     }
 
@@ -70,14 +66,11 @@ const Create = () => {
         <div className='create'>
             <Navbar />
             <div className="createContainer">
-
                 <div className="picsContainer">
-
                     <div className="formInput">
                         <h2>Upload Images (Max 3)</h2>
                         <label htmlFor="file">
-                            <FontAwesomeIcon
-                                className="icon" icon={faPlusCircle} />
+                            <FontAwesomeIcon className="icon" icon={faPlusCircle} />
                         </label>
                         <input
                             type="file"
@@ -88,41 +81,16 @@ const Create = () => {
                         />
                     </div>
                     <div className="uploadedPictures">
-                        <div className="upload_pic">
-                            <img
-                                src={
-                                    files[0]
-                                        ? URL.createObjectURL(files[0])
-                                        : ""
-                                }
-                                alt=""
-                                height="80px"
-                            />
-                        </div>
-                        <div className="upload_pic">
-                            <img
-                                src={
-                                    files[1]
-                                        ? URL.createObjectURL(files[1])
-                                        : ""
-                                }
-                                alt=""
-                                height="80px"
-                            />
-                        </div>
-                        <div className="upload_pic">
-                            <img
-                                src={
-                                    files[2]
-                                        ? URL.createObjectURL(files[2])
-                                        : ""
-                                }
-                                alt=""
-                                height="80px"
-                            />
-                        </div>
+                        {Array.from(files).map((file, index) => (
+                            <div className="upload_pic" key={index}>
+                                <img
+                                    src={URL.createObjectURL(file)}
+                                    alt=""
+                                    height="80px"
+                                />
+                            </div>
+                        ))}
                     </div>
-
                 </div>
 
                 <div className="input">
@@ -135,9 +103,7 @@ const Create = () => {
                     />
                 </div>
                 <div className="input">
-                    <label htmlFor="title">
-                        Location
-                    </label>
+                    <label htmlFor="location">Location</label>
                     <input
                         onChange={handleChange}
                         type="text"
@@ -147,9 +113,7 @@ const Create = () => {
                 </div>
 
                 <div className="input">
-                    <label htmlFor="date">
-                        What is the Date
-                    </label>
+                    <label htmlFor="date">What is the Date</label>
                     <input
                         onChange={handleChange}
                         type="date"
@@ -159,9 +123,7 @@ const Create = () => {
                 </div>
 
                 <div className="input">
-                    <label htmlFor="entry">
-                        Write your thoughts..
-                    </label>
+                    <label htmlFor="text">Write your thoughts..</label>
                     <textarea
                         name='entry'
                         id='text'
@@ -172,13 +134,13 @@ const Create = () => {
                     ></textarea>
                 </div>
 
-                <button className='createBtn'
-                    onClick={handleClick}>
+                <button className='createBtn' onClick={handleClick}>
                     Create Entry
                 </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default Create
+export default Create;
+
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,31 @@
 // client/src/pages/Home.jsx
 
-import React, {
-    useContext,
-    useState
-} from 'react'
-import Navbar from '../components/Navbar'
-import {
-    faMagnifyingGlass
-} from "@fortawesome/free-solid-svg-icons";
-import {
-    FontAwesomeIcon
-} from "@fortawesome/react-fontawesome";
-import useFetch from "../useFetch"
-import {
-    AuthContext
-} from '../authContext';
-import '../styles/home.css'
+import React, { useContext, useState } from 'react';
+import Navbar from '../components/Navbar';
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useFetch from "../useFetch";
+import { AuthContext } from '../authContext';
+import '../styles/home.css';
 import Card from '../components/Card';
 
 const Home = () => {
     const [query, setQuery] = useState("");
-    const { user } = useContext(AuthContext)
-    const { data, loading } = useFetch(
-        `/entries/author/${user._id}`)
+    const { user } = useContext(AuthContext);
+    const { data, loading } = useFetch(`/entries/author/${user._id}`);
 
     const keys = ["title", "location", "date"];
 
+    // Function to handle the search filtering
     const search = (data) => {
+        if (!Array.isArray(data)) {
+            return []; // Return empty array if data is not an array
+        }
         return data.filter((item) =>
             keys.some((key) => item[key] &&
-                item[key].toLowerCase().includes(query))
+                item[key].toLowerCase().includes(query.toLowerCase()))
         );
     };
-
 
     return (
         <div>
@@ -48,40 +41,44 @@ const Home = () => {
                         />
                         <FontAwesomeIcon
                             className="icon"
-                            icon={faMagnifyingGlass} />
+                            icon={faMagnifyingGlass}
+                        />
                     </div>
                 </div>
             </div>
 
             <div className="searchedPosts">
                 {loading ? (
-                    <>
-                        <div className="p"
-                            style={{
-                                color: "white", "fontFamily":
-                                    "'Kaushan Script', cursive"
-                            }}>
-                            Loading...
-                        </div>
-                    </>
+                    <div className="p"
+                        style={{
+                            color: "white", 
+                            fontFamily: "'Kaushan Script', cursive"
+                        }}>
+                        Loading...
+                    </div>
                 ) : (
                     <>
-                        {search(data)?.map((item, i) => (
-                            <Card
-                                key={i} // Remember to add a unique key
-                                _id={item._id}
-                                photos={item.photos}
-                                title={item.title}
-                                date={item.date}
-                                location={item.location}
-                                text={item.text}
-                            />
-                        ))}
+                        {Array.isArray(data) && data.length > 0 ? (
+                            search(data).map((item, i) => (
+                                <Card
+                                    key={i} // Ensure each Card has a unique key
+                                    _id={item._id}
+                                    photos={item.photos}
+                                    title={item.title}
+                                    date={item.date}
+                                    location={item.location}
+                                    text={item.text}
+                                />
+                            ))
+                        ) : (
+                            <div>No entries found</div> // Handle the case where no entries are found
+                        )}
                     </>
                 )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
+
